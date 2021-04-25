d333ffb00c553ae01630a0c50e5369abfd71f23a
Márcio Gusmão <marciogusmao@msn.com>
2021-04-25 07:58:13 -0300

    Update MediaEscolar.cs

--- MediaEscolar.cs
+++ MediaEscolar.cs
@@ -2,19 +2,23 @@
 using System;
 class MediaEscolar {
   static void Main() {
-      
+    
+    // Recebe a primeira nota
     Console.Write("Qual a primeira nota? ");
     double n1 = double.Parse(Console.ReadLine());
+    // Recebe a segunda nota
     Console.Write("Qual a segunda nota? ");
     double n2 = double.Parse(Console.ReadLine());
+     // Recebe a terceira nota
     Console.Write("Qual a terceira nota? ");
     double n3 = double.Parse(Console.ReadLine());
+    // Recebe a quarta nota
     Console.Write("Qual a quarta nota? ");
     double n4 = double.Parse(Console.ReadLine());
+    // Calcula a média
     double S = (n1+n2+n3+n4)/4;
-    
     Console.WriteLine("A média é: " + S);
-    
+    // Condiciona a situação do aluno, de acordo com sua média
     if (S>=7){
         Console.WriteLine("Aluno Aprovado!");
     } 
